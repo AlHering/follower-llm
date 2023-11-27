3f689f6782c2dd05ea01ec37a6fb315288f805e1
Alexander Hering <dev.hering@gmail.com>
2023-11-27 22:47:22 +0100

    starting coder class

--- src/model/ml_model_control/scraping_coder.py
+++ src/model/ml_model_control/scraping_coder.py
@@ -5,3 +5,10 @@
 *            (c) 2023 Alexander Hering             *
 ****************************************************
 """
+
+
+class ScrapingCoder(object):
+    """
+    Class, represinting Scraping Coders which utilize language models to support programming scraping infrastructure.
+    """
+    pass
