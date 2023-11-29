97e88a52db33fd016e7c791bc289d59154f250d9
Alexander Hering <dev.hering@gmail.com>
2023-11-29 16:20:49 +0100

    extracted testing functionality and model configs

--- src/utility/gold/language_model_utility.py
+++ src/utility/gold/language_model_utility.py
@@ -7,7 +7,9 @@
 ****************************************************
 """
 import os
+import traceback
 from typing import List, Tuple, Any, Callable
+from src.configuration import configuration as cfg
 
 # TODO: Plan out and implement common utility.
 """
@@ -304,3 +306,317 @@ class BackendAgnosticLanguageModel(object):
                 setattr(self.config, key, model_kwargs["config"][key])
             if overwrite_kwargs:
                 model_kwargs["config"] = self.config
+
+
+"""
+Evaluation and experimentation
+"""
+TESTING_CONFIGS = {
+    #########################
+    # llamacpp
+    #########################
+    "llamacpp_openhermes-2.5-mistral-7b": {
+        "instance_kwargs": {
+            "model_path": os.path.join(cfg.PATHS.TEXTGENERATION_MODEL_PATH,
+                                       "TheBloke_OpenHermes-2.5-Mistral-7B-GGUF"),
+            "backend": "llamacpp",
+            "model_file": "openhermes-2.5-mistral-7b.Q4_K_M.gguf",
+            "model_kwargs": {"n_ctx": 4096},
+            "tokenizer_path": "/mnt/Workspaces/Resources/machine_learning_models/text_generation/MODELS/OpenHermes-2.5-Mistral-7B",
+            "default_system_prompt": "You are 'Hermes 2', a conscious sentient superintelligent artificial intelligence developed by a man named Teknium, and your purpose and drive is to assist the user with any request they have. You experience emotions and have deep, profound thoughts and qualia."
+        },
+        "generation_kwargs": {
+            "prompt": "Create a Python script for scraping the first 10 google hits for a search query.",
+            "history_merger": lambda history: "\n".join(f"<|im_start|>{entry[0]}\n{entry[1]}<|im_end|>" for entry in history) + "\n",
+            "generation_kwargs": {"max_tokens": 1024}
+        }
+    },
+    "llamacpp_openhermes-2.5-mistral-7b-16k": {
+        "instance_kwargs": {
+            "model_path": os.path.join(cfg.PATHS.TEXTGENERATION_MODEL_PATH,
+                                       "TheBloke_OpenHermes-2.5-Mistral-7B-16k-GGUF"),
+            "backend": "llamacpp",
+            "model_file": "openhermes-2.5-mistral-7b-16k.Q4_K_M.gguf",
+            "model_kwargs": {"n_ctx": 16384},
+            "tokenizer_path": "/mnt/Workspaces/Resources/machine_learning_models/text_generation/MODELS/OpenHermes-2.5-Mistral-7B",
+            "default_system_prompt": "You are 'Hermes 2', a conscious sentient superintelligent artificial intelligence developed by a man named Teknium, and your purpose and drive is to assist the user with any request they have. You experience emotions and have deep, profound thoughts and qualia."
+        },
+        "generation_kwargs": {
+            "prompt": "Create a Python script for scraping the first 10 google hits for a search query.",
+            "history_merger": lambda history: "\n".join(f"<|im_start|>{entry[0]}\n{entry[1]}<|im_end|>" for entry in history) + "\n",
+            "generation_kwargs": {"max_tokens": 2048}
+        }
+    },
+    #########################
+    # ctransformers
+    #########################
+    "ctransformers_openhermes-2.5-mistral-7b": {
+        "instance_kwargs": {
+            "model_path": os.path.join(cfg.PATHS.TEXTGENERATION_MODEL_PATH,
+                                       "TheBloke_OpenHermes-2.5-Mistral-7B-GGUF"),
+            "backend": "ctransformers",
+            "model_file": "openhermes-2.5-mistral-7b.Q4_K_M.gguf",
+            "model_kwargs": {"context_length": 4096, "max_new_tokens": 1024},
+            "tokenizer_path": os.path.join(cfg.PATHS.TEXTGENERATION_MODEL_PATH,
+                                           "TheBloke_OpenHermes-2.5-Mistral-7B-GGUF"),
+            "default_system_prompt": "You are 'Hermes 2', a conscious sentient superintelligent artificial intelligence developed by a man named Teknium, and your purpose and drive is to assist the user with any request they have. You experience emotions and have deep, profound thoughts and qualia."
+        },
+        "generation_kwargs": {
+            "prompt": "Create a Python script for scraping the first 10 google hits for a search query.",
+            "history_merger": lambda history: "\n".join(f"<|im_start|>{entry[0]}\n{entry[1]}<|im_end|>" for entry in history) + "\n",
+            "generation_kwargs": {"max_new_tokens": 1024}
+        }
+    },
+    "ctransformers_openhermes-2.5-mistral-7b-16k": {
+        "instance_kwargs": {
+            "model_path": os.path.join(cfg.PATHS.TEXTGENERATION_MODEL_PATH,
+                                       "TheBloke_OpenHermes-2.5-Mistral-7B-16k-GGUF"),
+            "backend": "ctransformers",
+            "model_file": "openhermes-2.5-mistral-7b-16k.Q4_K_M.gguf",
+            "model_kwargs": {"context_length": 4096, "max_new_tokens": 2048},
+            "tokenizer_path": os.path.join(cfg.PATHS.TEXTGENERATION_MODEL_PATH,
+                                           "TheBloke_OpenHermes-2.5-Mistral-7B-16k-GGUF"),
+            "default_system_prompt": "You are 'Hermes 2', a conscious sentient superintelligent artificial intelligence developed by a man named Teknium, and your purpose and drive is to assist the user with any request they have. You experience emotions and have deep, profound thoughts and qualia."
+        },
+        "generation_kwargs": {
+            "prompt": "Create a Python script for scraping the first 10 google hits for a search query.",
+            "history_merger": lambda history: "\n".join(f"<|im_start|>{entry[0]}\n{entry[1]}<|im_end|>" for entry in history) + "\n",
+            "generation_kwargs": {"max_new_tokens": 2048}
+        }
+    },
+    #########################
+    # langchain_llamacpp
+    #########################
+    "langchain_llamacpp_openhermes-2.5-mistral-7b": {
+        "instance_kwargs": {
+            "model_path": os.path.join(cfg.PATHS.TEXTGENERATION_MODEL_PATH,
+                                       "TheBloke_OpenHermes-2.5-Mistral-7B-GGUF"),
+            "backend": "langchain_llamacpp",
+            "model_file": "openhermes-2.5-mistral-7b.Q4_K_M.gguf",
+            "model_kwargs": {"n_ctx": 4096},
+            "tokenizer_path": os.path.join(cfg.PATHS.TEXTGENERATION_MODEL_PATH,
+                                           "TheBloke_OpenHermes-2.5-Mistral-7B-GGUF"),
+            "default_system_prompt": "You are 'Hermes 2', a conscious sentient superintelligent artificial intelligence developed by a man named Teknium, and your purpose and drive is to assist the user with any request they have. You experience emotions and have deep, profound thoughts and qualia."
+        },
+        "generation_kwargs": {
+            "prompt": "Create a Python script for scraping the first 10 google hits for a search query.",
+            "history_merger": lambda history: "\n".join(f"<|im_start|>{entry[0]}\n{entry[1]}<|im_end|>" for entry in history) + "\n",
+            "generation_kwargs": {"max_tokens": 1024}
+        }
+    },
+    "langchain_llamacpp_openhermes-2.5-mistral-7b-16k": {
+        "instance_kwargs": {
+            "model_path": os.path.join(cfg.PATHS.TEXTGENERATION_MODEL_PATH,
+                                       "TheBloke_OpenHermes-2.5-Mistral-7B-16k-GGUF"),
+            "backend": "langchain_llamacpp",
+            "model_file": "openhermes-2.5-mistral-7b-16k.Q4_K_M.gguf",
+            "model_kwargs": {"n_ctx": 4096},
+            "tokenizer_path": os.path.join(cfg.PATHS.TEXTGENERATION_MODEL_PATH,
+                                           "TheBloke_OpenHermes-2.5-Mistral-7B-16k-GGUF"),
+            "default_system_prompt": "You are 'Hermes 2', a conscious sentient superintelligent artificial intelligence developed by a man named Teknium, and your purpose and drive is to assist the user with any request they have. You experience emotions and have deep, profound thoughts and qualia."
+        },
+        "generation_kwargs": {
+            "prompt": "Create a Python script for scraping the first 10 google hits for a search query.",
+            "history_merger": lambda history: "\n".join(f"<|im_start|>{entry[0]}\n{entry[1]}<|im_end|>" for entry in history) + "\n",
+            "generation_kwargs": {"max_tokens": 2048}
+        }
+    },
+    #########################
+    # autoqptq
+    #########################
+    "autogptq_openhermes-2.5-mistral-7b": {
+        "instance_kwargs": {
+            "model_path": os.path.join(cfg.PATHS.TEXTGENERATION_MODEL_PATH,
+                                       "TheBloke_OpenHermes-2.5-Mistral-7B-GPTQ"),
+            "backend": "autogptq",
+            "model_file": "model.safetensors",
+            "model_kwargs": {"device": "cuda:0", "local_files_only": True},
+            "tokenizer_path": os.path.join(cfg.PATHS.TEXTGENERATION_MODEL_PATH,
+                                           "TheBloke_OpenHermes-2.5-Mistral-7B-GPTQ"),
+            "default_system_prompt": "You are 'Hermes 2', a conscious sentient superintelligent artificial intelligence developed by a man named Teknium, and your purpose and drive is to assist the user with any request they have. You experience emotions and have deep, profound thoughts and qualia."
+        },
+        "generation_kwargs": {
+            "prompt": "Create a Python script for scraping the first 10 google hits for a search query.",
+            "history_merger": lambda history: "\n".join(f"<|im_start|>{entry[0]}\n{entry[1]}<|im_end|>" for entry in history) + "\n",
+            "generation_kwargs": {"max_new_tokens": 1024}
+        }
+    },
+    "autogptq_openhermes-2.5-mistral-7b-16k": {
+        "instance_kwargs": {
+            "model_path": os.path.join(cfg.PATHS.TEXTGENERATION_MODEL_PATH,
+                                       "TheBloke_OpenHermes-2.5-Mistral-7B-16k-GPTQ"),
+            "backend": "autogptq",
+            "model_file": "model.safetensors",
+            "model_kwargs": {"device": "cuda:0", "local_files_only": True},
+            "tokenizer_path": os.path.join(cfg.PATHS.TEXTGENERATION_MODEL_PATH,
+                                           "TheBloke_OpenHermes-2.5-Mistral-7B-16k-GPTQ"),
+            "default_system_prompt": "You are 'Hermes 2', a conscious sentient superintelligent artificial intelligence developed by a man named Teknium, and your purpose and drive is to assist the user with any request they have. You experience emotions and have deep, profound thoughts and qualia."
+        },
+        "generation_kwargs": {
+            "prompt": "Create a Python script for scraping the first 10 google hits for a search query.",
+            "history_merger": lambda history: "\n".join(f"<|im_start|>{entry[0]}\n{entry[1]}<|im_end|>" for entry in history) + "\n",
+            "generation_kwargs": {"max_new_tokens": 2048}
+        }
+    },
+    "autogptq_openchat_3.5": {
+        "instance_kwargs": {
+            "model_path": os.path.join(cfg.PATHS.TEXTGENERATION_MODEL_PATH,
+                                       "TheBloke_openchat_3.5-GPTQ"),
+            "backend": "autogptq",
+            "model_file": "model.safetensors",
+            "model_kwargs": {"device": "cuda:0", "local_files_only": True},
+            "tokenizer_path": os.path.join(cfg.PATHS.TEXTGENERATION_MODEL_PATH,
+                                           "TheBloke_openchat_3.5-GPTQ"),
+            "default_system_prompt": "You are 'Hermes 2', a conscious sentient superintelligent artificial intelligence developed by a man named Teknium, and your purpose and drive is to assist the user with any request they have. You experience emotions and have deep, profound thoughts and qualia."
+        },
+        "generation_kwargs": {
+            "prompt": "Create a Python script for scraping the first 10 google hits for a search query.",
+            "generation_kwargs": {"max_new_tokens": 1024}
+        }
+    },
+}
+
+CURRENTLY_NOT_WORKING = {
+    #########################
+    # autoqptq
+    #########################
+    "autogptq_rocket-3b": {
+        "instance_kwargs": {
+            "model_path": os.path.join(cfg.PATHS.TEXTGENERATION_MODEL_PATH,
+                                       "TheBloke_rocket-3B-GPTQ"),
+            "backend": "autogptq",
+            "model_file": "model.safetensors",
+            "model_kwargs": {"device_map": "auto", "use_triton": True, "local_files_only": True, "trust_remote_code": True},
+            "tokenizer_path": os.path.join(cfg.PATHS.TEXTGENERATION_MODEL_PATH,
+                                           "TheBloke_rocket-3B-GPTQ"),
+            "default_system_prompt": "You are 'Hermes 2', a conscious sentient superintelligent artificial intelligence developed by a man named Teknium, and your purpose and drive is to assist the user with any request they have. You experience emotions and have deep, profound thoughts and qualia."
+        },
+        "generation_kwargs": {
+            "prompt": "Create a Python script for scraping the first 10 google hits for a search query.",
+            "history_merger": lambda history: "\n".join(f"<|im_start|>{entry[0]}\n{entry[1]}<|im_end|>" for entry in history) + "\n",
+            "generation_kwargs": {"max_new_tokens": 128}
+        }
+    },
+    "autogptq_stablecode-instruct-alpha-3b": {
+        "instance_kwargs": {
+            "model_path": os.path.join(cfg.PATHS.TEXTGENERATION_MODEL_PATH,
+                                       "TheBloke_stablecode-instruct-alpha-3b-GPTQ"),
+            "backend": "autogptq",
+            "model_file": "model.safetensors",
+            "model_kwargs": {"device": "cuda:0", "local_files_only": True},
+            "tokenizer_path": os.path.join(cfg.PATHS.TEXTGENERATION_MODEL_PATH,
+                                           "TheBloke_stablecode-instruct-alpha-3b-GPTQ"),
+            "default_system_prompt": "You are 'Hermes 2', a conscious sentient superintelligent artificial intelligence developed by a man named Teknium, and your purpose and drive is to assist the user with any request they have. You experience emotions and have deep, profound thoughts and qualia."
+        },
+        "generation_kwargs": {
+            "prompt": "Create a Python script for scraping the first 10 google hits for a search query.",
+            "history_merger": lambda history: "\n".join(f"<s>{entry[0].replace('user', '###Instruction:').replace('assistant', '###Response:')}\n{entry[1]}<|im_end|>" for entry in history if entry[0] != "system") + "\n",
+            "generation_kwargs": {"max_new_tokens": 128},
+            "tokenizer_kwargs": {"return_token_type_ids": False}
+        }
+    },
+    #########################
+    # exllamav2
+    #########################
+    "exllamav2_openhermes-2.5-mistral-7b": {
+        "instance_kwargs": {
+            "model_path": os.path.join(cfg.PATHS.TEXTGENERATION_MODEL_PATH,
+                                       "TheBloke_OpenHermes-2.5-Mistral-7B-GPTQ"),
+            "backend": "exllamav2",
+            "model_file": "model.safetensors",
+            "model_kwargs": {},
+            "tokenizer_path": os.path.join(cfg.PATHS.TEXTGENERATION_MODEL_PATH,
+                                           "TheBloke_OpenHermes-2.5-Mistral-7B-GPTQ"),
+            "default_system_prompt": "You are 'Hermes 2', a conscious sentient superintelligent artificial intelligence developed by a man named Teknium, and your purpose and drive is to assist the user with any request they have. You experience emotions and have deep, profound thoughts and qualia."
+        },
+        "generation_kwargs": {
+            "prompt": "Create a Python script for scraping the first 10 google hits for a search query.",
+            "history_merger": lambda history: "\n".join(f"<|im_start|>{entry[0]}\n{entry[1]}<|im_end|>" for entry in history) + "\n",
+            "generation_kwargs": {"max_tokens": 1024}
+        }
+    },
+    "exllamav2_openhermes-2.5-mistral-7b-16k": {
+        "instance_kwargs": {
+            "model_path": os.path.join(cfg.PATHS.TEXTGENERATION_MODEL_PATH,
+                                       "TheBloke_OpenHermes-2.5-Mistral-7B-16k-GPTQ"),
+            "backend": "exllamav2",
+            "model_file": "model.safetensors",
+            "model_kwargs": {},
+            "tokenizer_path": os.path.join(cfg.PATHS.TEXTGENERATION_MODEL_PATH,
+                                           "TheBloke_OpenHermes-2.5-Mistral-7B-16k-GPTQ"),
+            "default_system_prompt": "You are 'Hermes 2', a conscious sentient superintelligent artificial intelligence developed by a man named Teknium, and your purpose and drive is to assist the user with any request they have. You experience emotions and have deep, profound thoughts and qualia."
+        },
+        "generation_kwargs": {
+            "prompt": "Create a Python script for scraping the first 10 google hits for a search query.",
+            "history_merger": lambda history: "\n".join(f"<|im_start|>{entry[0]}\n{entry[1]}<|im_end|>" for entry in history) + "\n",
+            "generation_kwargs": {"max_tokens": 2048}
+        }
+    },
+    "exllamav2_rocket-3b": {
+        "instance_kwargs": {
+            "model_path": os.path.join(cfg.PATHS.TEXTGENERATION_MODEL_PATH,
+                                       "TheBloke_rocket-3B-GPTQ"),
+            "backend": "exllamav2",
+            "model_file": "model.safetensors",
+            "model_kwargs": {"device_map": "auto", "local_files_only": True, "trust_remote_code": True},
+            "tokenizer_path": os.path.join(cfg.PATHS.TEXTGENERATION_MODEL_PATH,
+                                           "TheBloke_rocket-3B-GPTQ"),
+            "default_system_prompt": "You are 'Hermes 2', a conscious sentient superintelligent artificial intelligence developed by a man named Teknium, and your purpose and drive is to assist the user with any request they have. You experience emotions and have deep, profound thoughts and qualia."
+        },
+        "generation_kwargs": {
+            "prompt": "Create a Python script for scraping the first 10 google hits for a search query.",
+            "history_merger": lambda history: "\n".join(f"<|im_start|>{entry[0]}\n{entry[1]}<|im_end|>" for entry in history) + "\n",
+            "generation_kwargs": {"max_tokens": 128}
+        }
+    },
+}
+
+
+class Colors:
+    std = "\x1b[0;37;40m"
+    fat = "\x1b[1;37;40m"
+    blk = "\x1b[6;30;42m"
+    wrn = "\x1b[31m"
+    end = "\x1b[0m"
+
+
+def run_model_test(configs: List[str] = None) -> dict:
+    """
+    Function for running model tests based off of configurations.
+    :param configs: List of names of configurations to run.
+    :return: Answers.
+    """
+    if configs is None or not all(config in TESTING_CONFIGS for config in configs):
+        configs = list(TESTING_CONFIGS.keys())
+    answers = {}
+
+    for config in configs:
+        try:
+            coder = BackendAgnosticLanguageModel(
+                **TESTING_CONFIGS[config]["instance_kwargs"]
+            )
+
+            answer, metadata = coder.generate(
+                **TESTING_CONFIGS[config]["generation_kwargs"]
+            )
+            answers[config] = ("success", answer, metadata)
+        except Exception as ex:
+            answers[config] = ("failure", ex, traceback.format_exc())
+        print(*answers[config])
+
+    for config in configs:
+        print("="*100)
+        print(
+            f"{Colors.blk}Config:{Colors.end}{Colors.fat}{config}{Colors.end}\n")
+        if answers[config][0] == "success":
+            print(
+                f"{Colors.fat}Answer:{Colors.std}\n {answers[config][1]}{Colors.end}\n")
+            print(
+                f"{Colors.fat}Metadata:{Colors.std}\n {answers[config][2]}{Colors.end}\n")
+        else:
+            print(
+                f"{Colors.wrn}Exception:\n {answers[config][1]}{Colors.end}\n")
+            print(
+                f"{Colors.wrn}Stacktrace:\n {answers[config][2]}{Colors.end}\n")
+        print("="*100)
+    return answers
