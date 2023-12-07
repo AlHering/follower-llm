185056061fcc78969f156491eec3cee323ec4e86
Alexander Hering <dev.hering@gmail.com>
2023-12-07 23:23:38 +0100

    starting scraping plugin

---
+++ src/model/scraping/scraping_plugins.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+****************************************************
+*                   Follower LLM                   *
+*            (c) 2023 Alexander Hering             *
+****************************************************
+"""
+from src.model.plugin_model.plugins import GenericPlugin, PluginImportException, PluginRuntimeException
+
+
+class ScrapingPlugin(GenericPlugin):
+    """
+    Class, representing a scraping plugin.
+    """
+    pass
